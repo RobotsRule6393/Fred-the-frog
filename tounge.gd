3d7cc123c4f3404db9993e8896bfff1108367f4f
RobotsRule6393 <teamnwfalcon@gmail.com>
2026-03-28 16:46:06 +1300

    Update tounge.gd

--- tounge.gd
+++ tounge.gd
@@ -1,12 +1,8 @@
 extends Area2D
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$".".scale = Vector2(0,0)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("click"):
 		look_at(get_global_mouse_position())
